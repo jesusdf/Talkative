1847b39c2a768a58d109ec9542ee2c957c4d78ed
JesusDF <jesusdf@gmail.com>
2012-02-18 14:11:57 +0100

    Add license information.

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("vik")]
+[assembly: AssemblyCopyright("JesusDF")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
